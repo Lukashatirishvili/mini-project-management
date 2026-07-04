49e37964c8bddba31b2ff71d360019218ab38753
Luka Shatirishvili <lukkashatirishvili@gmail.com>
2026-07-04 19:41:50 +0400

    Add Ef core database and initial migration

--- MiniProjectManagement.Api/Program.cs
+++ MiniProjectManagement.Api/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MiniProjectManagement.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Built-in OpenAPI document generation
 builder.Services.AddOpenApi();
